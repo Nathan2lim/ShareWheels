abcda0eb96c2ec6c26e91371e03f104a8fce9171
Nathan2lim <74240315+Nathan2lim@users.noreply.github.com>
2025-05-19 16:23:02 +0200

    feat:

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ rm -rf /srv/ShareWheels/staticfiles
 echo "3. Nettoyage du dossier 'images'..."
 rm -rf /srv/ShareWheels/images
 
-echo "3. Nettoyage du dossier 'event_photos'..."
+echo "4. Nettoyage du dossier 'event_photos'..."
 rm -rf /srv/ShareWheels/event_photos
 
 # Fonction pour remplacer les espaces dans les noms de fichiers d'un dossier
@@ -27,25 +27,25 @@ replace_spaces() {
     done
 }
 
-echo "3. Renommage des fichiers avec des espaces..."
+echo "5. Renommage des fichiers avec des espaces..."
 # Remplacer les espaces dans les dossiers statiques
 replace_spaces "/srv/ShareWheels/app/static"
 replace_spaces "/srv/ShareWheels/applicompte/static"
 replace_spaces "/srv/ShareWheels/carte/static"
 
-echo "4. Création des migrations pour l'application 'app'..."
+echo "6. Création des migrations pour l'application 'app'..."
 docker compose exec web python manage.py makemigrations app
 
-echo "5. Création des migrations pour l'application 'carte'..."
+echo "7. Création des migrations pour l'application 'carte'..."
 docker compose exec web python manage.py makemigrations carte
 
-echo "6. Reconstruire et démarrer les conteneurs..."
+echo "8. Reconstruire et démarrer les conteneurs..."
 docker compose up -d --build
 
-echo "7. Application des migrations..."
+echo "9. Application des migrations..."
 docker compose exec web python manage.py migrate --noinput
 
-echo "8. Collecte des fichiers statiques..."
+echo "10. Collecte des fichiers statiques..."
 docker compose exec web python manage.py collectstatic --noinput
 
 echo "✅ Déploiement terminé $(date)"
